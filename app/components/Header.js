f74c9f3ce12a402d8920af6314ce36acf30dac78
Markus Averstad <markus.averstad@axfood.se>
2017-07-19 15:43:59 +0200

    Added banner

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1>Comic Rocket</h1>
+        <img src="img/reader-logo.png" alt="Comic Rocket Reader" />
       </header>
     );
   }

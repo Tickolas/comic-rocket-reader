efc139eb60ac1b78690a2350db22de073e94e708
Markus Averstad <markus.averstad@axfood.se>
2017-07-21 14:18:12 +0200

    Finished renaming

---
+++ app/api/Login.js
@@ -0,0 +1,18 @@
+import http from 'axios';
+import paths from './Paths';
+
+function isLoggedIn() {
+  return http.get(paths.LOGIN_CHECK).then(() => {
+    return true;
+  }).catch((error) => {
+    if (error.response.status === 401) {
+      return Promise.resolve(false);
+    } else {
+      throw new Error('Unknown error');
+    }
+  });
+}
+
+module.exports = {
+  isLoggedIn
+};

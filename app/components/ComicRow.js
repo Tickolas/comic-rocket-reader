cc3f04f6293bc47d46068c9ba8951b955e1ad8f3
Markus Averstad <markus.averstad@gmail.com>
2018-05-16 17:09:13 +0200

    Fix broken open tab function

--- app/components/ComicRow.js
+++ app/components/ComicRow.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
-import { openNewTabFor } from '../utils/ChromeUtils'
+import { openSingleNewTabFor } from '../utils/ChromeUtils'
 
 const ComicRow = ({comic}) => {
   function getBanner () {
-    if(comic.banner_url) {
+    if (comic.banner_url) {
       return <img src={comic.banner_url} alt={comic.name} />
     } else {
       return <h2>{comic.name}</h2>
@@ -13,7 +13,7 @@ const ComicRow = ({comic}) => {
 
   return (
     <div>
-      <div onClick={() => openNewTabFor(comic, true)}>
+      <div onClick={() => openSingleNewTabFor(comic, true)}>
         {getBanner()}
       </div>
       <div>[ {comic.idx} / {comic.max_idx} ]</div>

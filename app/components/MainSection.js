89f48b124d62ea18259ba815225d4e3abf9729f4
Markus Averstad <markus.averstad@gmail.com>
2018-05-16 16:27:45 +0200

    Make MainSection stateless.

--- app/components/MainSection.js
+++ app/components/MainSection.js
@@ -1,40 +1,17 @@
-import React, { Component } from 'react'
-import Comics from '../api/Comics'
-import Login from '../api/Login'
+import React from 'react'
+import { PropTypes } from 'prop-types'
 import ComicsList from './ComicsList'
-import { filterUnread } from '../utils/ComicsUtil'
-import { updateBadgeText } from '../utils/BadgeUtils'
 
-export default class MainSection extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      isLoggedIn: false,
-      unreadComics: []
-    }
-  }
-
-  componentDidMount () {
-    Login.isLoggedIn().then((isLoggedIn) => {
-      this.setState({ isLoggedIn })
-    })
-
-    Comics.get().then((comics) => {
-      this.setState({ unreadComics: filterUnread(comics) })
-      updateBadgeText(comics)
-    })
-  }
-
-  render () {
-    return (
-      <section>
-        {
-          this.state.isLoggedIn
-            ? <ComicsList comics={this.state.unreadComics} />
-            : <div>{'Please do log in.'}</div>
-        }
-      </section>
-    )
-  }
+const MainSection = ({comics}) => {
+  return (
+    <section>
+      <ComicsList comics={comics} />
+    </section>
+  )
 }
+
+MainSection.propTypes = {
+  comics: PropTypes.array.isRequired
+}
+
+export default MainSection
--- app/containers/App.js
+++ app/containers/App.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react'
 import Header from '../components/Header'
 import MainSection from '../components/MainSection'
 import Footer from '../components/Footer'
+import Login from '../api/Login'
+import Comics from '../api/Comics'
+import { updateBadgeText } from '../utils/BadgeUtils'
+import { filterUnread } from '../utils/ComicsUtil'
 
 export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isLoggedIn: false,
+      unreadComics: []
+    }
+  }
+
+  componentDidMount () {
+    Login.isLoggedIn().then((isLoggedIn) => {
+      this.setState({isLoggedIn})
+    })
+
+    Comics.get().then((comics) => {
+      this.setState({unreadComics: filterUnread(comics)})
+      updateBadgeText(comics)
+    })
+  }
+
   render () {
     return (
       <div>
         <Header />
-        <MainSection />
-        <Footer />
+        {
+          this.state.isLoggedIn
+            ? <MainSection comics={this.state.unreadComics} />
+            : <div>{'Please do log in.'}</div>
+        }
+        <Footer comics={this.state.unreadComics} isLoggedIn={this.state.isLoggedIn} />
       </div>
     )
   }

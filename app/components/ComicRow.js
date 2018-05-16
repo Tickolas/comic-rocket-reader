35d8a6b177c52ff8b7eb61774c1a1314948b94ed
Markus Averstad <markus.averstad@gmail.com>
2018-05-16 16:47:25 +0200

    Quick and dirty banners if banner url is missing

--- app/components/ComicRow.js
+++ app/components/ComicRow.js
@@ -3,10 +3,18 @@ import { PropTypes } from 'prop-types'
 import { openNewTabFor } from '../utils/ChromeUtils'
 
 const ComicRow = ({comic}) => {
+  function getBanner () {
+    if(comic.banner_url) {
+      return <img src={comic.banner_url} alt={comic.name} />
+    } else {
+      return <h2>{comic.name}</h2>
+    }
+  }
+
   return (
     <div>
       <div onClick={() => openNewTabFor(comic, true)}>
-        <img src={comic.banner_url} alt={comic.name} />
+        {getBanner()}
       </div>
       <div>[ {comic.idx} / {comic.max_idx} ]</div>
     </div>

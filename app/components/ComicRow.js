2d87e0e50add78e868fbd9e3e4f79e4917d1034e
Markus Averstad <markus.averstad@gmail.com>
2018-06-04 12:31:51 +0200

    Add nicer backlog buttons.

--- app/components/ComicRow.js
+++ app/components/ComicRow.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
 import { openSingleNewTabFor } from '../utils/ChromeUtils'
-import ComicRowStyle from './ComicRow.css'
+import style from './ComicRow.css'
 import { connect } from 'react-redux'
 import { ADD_COMIC_TO_BACKLOG, REMOVE_COMIC_FROM_BACKLOG } from '../constants/ActionTypes'
 
 const ComicRow = ({comic, backlog, addToBacklog, removeFromBacklog}) => {
   function getBanner () {
     if (comic.banner_url) {
-      return <img className={ComicRowStyle.comicRow__banner__image} src={comic.banner_url} alt={comic.name} />
+      return <img className={style.comicRow__banner__image} src={comic.banner_url} alt={comic.name} />
     } else {
-      return <div className={ComicRowStyle.comicRow__banner__placeholder}><div>{comic.name}</div></div>
+      return <div className={style.comicRow__banner__placeholder}><div>{comic.name}</div></div>
     }
   }
 
   function getBacklogButton () {
     if (!backlog.find(slug => slug === comic.slug)) {
-      return <div className={ComicRowStyle.comicRow__backlogButton} onClick={() => addToBacklog(comic)}>
-        <span>V</span>
+      return <div className={style.comicRow__backlogButton} onClick={() => addToBacklog(comic)}>
+        <img className={style.backlogButton__content} src='img/arrow-down.png' />
       </div>
     } else {
-      return <div className={ComicRowStyle.comicRow__backlogButton} onClick={() => removeFromBacklog(comic)}>
-        <span>^</span>
+      return <div className={style.comicRow__backlogButton} onClick={() => removeFromBacklog(comic)}>
+        <img className={style.backlogButton__content} src='img/arrow-up.png' />
       </div>
     }
   }
 
   return (
-    <div className={ComicRowStyle.comicRow}>
-      <div className={ComicRowStyle.comicRow__banner} onClick={() => openSingleNewTabFor(comic, true)}>
+    <div className={style.comicRow}>
+      <div className={style.comicRow__banner} onClick={() => openSingleNewTabFor(comic, true)}>
         {getBanner()}
       </div>
-      <div className={ComicRowStyle.comicRow__pages}>
-        <span className={ComicRowStyle.comicRow__pages__text}>{comic.idx} / {comic.max_idx}</span>
+      <div className={style.comicRow__pages}>
+        <span className={style.comicRow__pages__text}>{comic.idx} / {comic.max_idx}</span>
       </div>
       {
         getBacklogButton()

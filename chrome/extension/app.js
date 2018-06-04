4bf8e87e9c72403f2586b28720d4f136592c93f4
Markus Averstad <markus.averstad@gmail.com>
2018-06-04 11:28:10 +0200

    Remove obsolete storage interaction and background script

--- chrome/extension/app.js
+++ chrome/extension/app.js
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from '../../app/containers/Root'
 
-window.chrome.storage.local.get('state', () => {
-  ReactDOM.render(
-    <Root />,
-    document.querySelector('#root')
-  )
-})
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+)
--- chrome/extension/background/badge.js
+++
@@ -1,3 +0,0 @@
-/* global chrome */
-
-chrome.browserAction.setBadgeText({ text: '...' })

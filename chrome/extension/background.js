77339c05b8807accb06586f47c6940a1433b863f
Markus Averstad <markus.averstad@gmail.com>
2018-06-05 15:00:39 +0200

    Really stop overwriting backlog list

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -7,16 +7,17 @@ import { countUnreadPages } from '../../app/utils/ComicsUtil'
 const updateComics = () => {
   http.get(FETCH_COMICS).then((result) => {
     const unreadPages = countUnreadPages(result.data)
-    chrome.browserAction.setBadgeText({ text: unreadPages < 1000 ? '' + unreadPages : '...' })
-    chrome.storage.local.set({comicRocketReader: {
-      ...result.data.comicRocketReader,
-      comics: result.data
-    }})
+    chrome.browserAction.setBadgeText({ text: unreadPages < 1000 ? '' + unreadPages : 'Lots' })
+    chrome.storage.local.get('comicRocketReader', storage => {
+      const comicRocketReader = storage.comicRocketReader
+      comicRocketReader.comics = result.data
+      chrome.storage.local.set({comicRocketReader})
+    })
   })
 
   setTimeout(() => {
     updateComics()
-  }, 120000)
+  }, 4000)
 }
 
 updateComics()

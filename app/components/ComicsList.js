41e66e1c029c5e77d329e4770d98ffa0a8f28f8d
Markus Averstad <markus.averstad@gmail.com>
2018-06-05 10:53:47 +0200

    Implement BY_UNREAD_PAGES sort mode

--- app/components/ComicsList.js
+++ app/components/ComicsList.js
@@ -3,13 +3,17 @@ import { PropTypes } from 'prop-types'
 import ComicRow from './ComicRow'
 import style from './ComicsList.css'
 import { connect } from 'react-redux'
-import { BY_COMIC_NAME } from '../constants/SortModes'
+import { BY_COMIC_NAME, BY_UNREAD_PAGES } from '../constants/SortModes'
 
 const ComicsList = ({comics, sortOrder}) => {
   function sortFunction (comicA, comicB) {
     if (sortOrder === BY_COMIC_NAME) {
       if (comicA.name.toUpperCase() < comicB.name.toUpperCase()) { return -1 }
       if (comicA.name.toUpperCase() > comicB.name.toUpperCase()) { return 1 }
+      return 0
+    } else if (sortOrder === BY_UNREAD_PAGES) {
+      if ((comicA.max_idx - comicA.idx) < (comicB.max_idx - comicB.idx)) { return -1 }
+      if ((comicA.max_idx - comicA.idx) > (comicB.max_idx - comicB.idx)) { return 1 }
       return 0
     }
   }

59e8ee56a2fd95c62d2e11152e67329820597582
Markus Averstad <markus.averstad@gmail.com>
2018-06-07 10:31:48 +0200

    Fixed warning about "read only" settings

--- app/components/SettingsSection.js
+++ app/components/SettingsSection.js
@@ -11,23 +11,23 @@ const SettingsSection = ({changeSettings, settings}) => {
       <div>
         <p>Sort by: </p>
         <input type='radio' name='sortBy' onClick={() => changeSettings({sortMode: BY_COMIC_NAME})}
-          checked={settings.sortMode === BY_COMIC_NAME} /> Comic Name<br />
+          checked={settings.sortMode === BY_COMIC_NAME} readOnly /> Comic Name<br />
         <input type='radio' name='sortBy' onClick={() => changeSettings({sortMode: BY_UNREAD_PAGES})}
-          checked={settings.sortMode === BY_UNREAD_PAGES} /> Pages Left<br />
+          checked={settings.sortMode === BY_UNREAD_PAGES} readOnly /> Pages Left<br />
       </div>
       <div>
         <p>Reverse: </p>
         <input type='radio' name='sortOrder' onClick={() => changeSettings({reverseSort: true})}
-          checked={settings.reverseSort} /> Yes<br />
+          checked={settings.reverseSort} readOnly /> Yes<br />
         <input type='radio' name='sortOrder' onClick={() => changeSettings({reverseSort: false})}
-          checked={!settings.reverseSort} /> No<br />
+          checked={!settings.reverseSort} readOnly /> No<br />
       </div>
       <div>
         <p>How to show pages left: </p>
         <input type='radio' name='unreadPageMode' onClick={() => changeSettings({unreadPageMode: X_SLASH_Y})}
-          checked={settings.unreadPageMode === X_SLASH_Y} /> 42 / 1337<br />
+          checked={settings.unreadPageMode === X_SLASH_Y} readOnly /> 42 / 1337<br />
         <input type='radio' name='unreadPageMode' onClick={() => changeSettings({unreadPageMode: PAGES_LEFT})}
-          checked={settings.unreadPageMode === PAGES_LEFT} /> [ 44 ]<br />
+          checked={settings.unreadPageMode === PAGES_LEFT} readOnly /> [ 44 ]<br />
       </div>
     </section>
   )
